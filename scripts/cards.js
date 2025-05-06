36649cd15495b5537a8f78c1c3971cb365a6d750
Anton Kurushin <anton.kurushin2025@gmail.com>
2025-05-06 14:54:16 +0300

    feat: добавить попапы

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,6 +1,25 @@
-function createCard() {
+function createCard(card) {
     const cardTemp = document.querySelector('#card-template').content;
-    return cardTemp.querySelector('.card').cloneNode(true);
+    const cardEl = cardTemp.querySelector('.card').cloneNode(true);
+    const cardImage = cardEl.querySelector('.card__image');
+    const binButton = cardEl.querySelector('.card__delete-button');
+    const likeButton = cardEl.querySelector('.card__like-button');
+
+    cardEl.querySelector('.card__title').textContent = card.name;
+    cardEl.querySelector('.card__image').src = card.link;
+    cardEl.querySelector('.card__image').alt = card.alt;
+    placesList.prepend(cardEl);
+
+    cardImage.addEventListener('click', () => {
+        fillCardPopup(cardEl);
+    });
+    likeButton.addEventListener('click', () => {
+        likeButton.classList.toggle('card__like-button_is-active');
+    });
+
+    binButton.addEventListener('click', () => {
+        likeButton.closest('.card').remove();
+    });
 }
 
 const initialCards = [
@@ -32,10 +51,4 @@ const initialCards = [
 
 const placesList = document.querySelector('.places__list');
 
-initialCards.forEach((card) => {
-    const cardEl = createCard();
-    cardEl.querySelector('.card__title').textContent = card.name;
-    cardEl.querySelector('.card__image').src = card.link;
-    cardEl.querySelector('.card__image').alt = card.alt;
-    placesList.append(cardEl);
-})
+initialCards.reverse().forEach(createCard);
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,34 @@
 function openModal(popup) {
     popup.classList.add('popup_is-opened');
+    const popupCloseButton = popup.querySelector('.popup__close');
+    popupCloseButton.addEventListener('click', () => {closeModal(popup)});
 }
 
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
 }
 
-function fillPopup(popup) {
+function popupAnimationActivation() {
+    const popups = document.querySelectorAll('.popup');
+    debugger;
+    popups.forEach((popup) => {popup.classList.add('popup_is-animated')});
+}
+
+function fillProfilePopup(popup) {
     openModal(popup);
 
     nameInput.value = name.textContent;
     jobInput.value = description.textContent;
+}
+
+function fillCardPopup(card) {
+    openModal(imagePopup);
+
+    const placeName = card.querySelector('.card__description').textContent;
+    const photoLink = card.querySelector('.card__image').src;
+
+    imagePopupName.textContent = placeName;
+    imagePopupLink.src = photoLink;
 }
 
 function handleProfileFormSubmit(evt) {
@@ -21,24 +39,43 @@ function handleProfileFormSubmit(evt) {
     closeModal(profilePopup);
 }
 
+function handleCardFormSubmit(evt) {
+    evt.preventDefault();
+
+    const cardName = cardPopup.querySelector('.popup__input_type_card-name').value;
+    const cardLink = cardPopup.querySelector('.popup__input_type_url').value;
+
+    createCard({
+        name: cardName,
+        link: cardLink,
+    });
+
+    closeModal(cardPopup);
+}
+
 
 const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
-const profileFormElement  = document.querySelector('.popup__form');
+const profileFormElement  = profilePopup.querySelector('.popup__form');
+const cardFormElement = cardPopup.querySelector('.popup__form');
 
 const name = document.querySelector('.profile__title');
 const description = document.querySelector('.profile__description');
 const nameInput  = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
+const imagePopupName = imagePopup.querySelector('.popup__caption');
+const imagePopupLink = imagePopup.querySelector('.popup__image');
 
-const profileButton = document.querySelector('.profile__add-button');
+const cardAddButton = document.querySelector('.profile__add-button');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__close');
 
-profileEditButton.addEventListener('click', () => {fillPopup(profilePopup)});
-popupCloseButton.addEventListener('click', () => {closeModal(profilePopup)});
+popupAnimationActivation();
+
+profileEditButton.addEventListener('click', () => {fillProfilePopup(profilePopup)});
 profileFormElement .addEventListener('submit', handleProfileFormSubmit);
+cardFormElement.addEventListener('submit', handleCardFormSubmit);
+cardAddButton.addEventListener('click', () => {openModal(cardPopup)});
 
 
 // @todo: Темплейт карточки
